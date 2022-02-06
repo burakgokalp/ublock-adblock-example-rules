3fc0079f57a770c65430c7d3a4e7dc4d2886a5b2
burakgokalp <64479532+burakgokalp@users.noreply.github.com>
2022-02-06 18:51:08 +0300

    Update tempermonkeyScripts.js
    with vanilla javascript

--- tempermonkeyScripts.js
+++ tempermonkeyScripts.js
@@ -10,26 +10,50 @@
 // ==/UserScript==
 
 
-$(document).ready(function(){
-  $("tr[bgcolor='#FFFFFF']").remove();
-  $("tr[class='r-ustkonu']").remove();
-  $("tr[class='ustkonu']").remove();
-  $(".konulisteTextReklam").remove();
-  $("#Table7").remove();
-  $("td[width='13%'").remove();
-  $("td[width='10'").remove();
-  $("img[src^='http://adserve.donanimhaber'").parent().remove();
-  $("a[href='/m_94015223/tm.htm']").parent().parent().parent().parent().remove();
-  $("img[src='http://icon.donanimhaber.com/avantajx_buton.png']").parent().remove();
-  $("img[src='http://icon.donanimhaber.com/gfbuton.png']").parent().remove();
-  $(".sagtaraf").remove();
-  $("div[class*='Reklam']").remove();
-  $("div[class*='reklam']").remove();
-  closeCookieUsingPopup();
-  //$(".cookie-kutu").remove();
-  $("#notification-permission").remove();
 
-//linkleri düzeltelim.
+
+function ClearDHFromADS(){
+    console.log("selam");
+
+    function IfExistsRemove(selector){
+        var x = document.querySelector(selector);
+        if (x != null) {
+            x.remove();
+        }
+    }
+
+    IfExistsRemove("tr[bgcolor='#FFFFFF']");
+    IfExistsRemove("tr[class='r-ustkonu']");
+    IfExistsRemove("tr[class='ustkonu']");
+    IfExistsRemove(".konulisteTextReklam");
+    IfExistsRemove("#Table7");
+    IfExistsRemove("td[width='13%'");
+    IfExistsRemove("td[width='10'");
+    /*
+    $("img[src^='http://adserve.donanimhaber'").parent().remove();
+    $("a[href='/m_94015223/tm.htm']").parent().parent().parent().parent().remove();
+    $("img[src='http://icon.donanimhaber.com/avantajx_buton.png']").parent().remove();
+    $("img[src='http://icon.donanimhaber.com/gfbuton.png']").parent().remove();*/
+    IfExistsRemove(".sagtaraf");
+    IfExistsRemove("div[class*='Reklam']");
+    IfExistsRemove("div[class*='reklam']");
+    IfExistsRemove(".cookie-kutu");
+    IfExistsRemove("#notification-permission");
+
+    //linkleri düzeltelim.
+    /*
+    var links = document.getElementsByTagName("a"); // or document.getElementsByTagName("a");
+    for (var i=0, n=links.length; i<n; i++) {
+        if (links[i].href != null) {
+            console.log(links[i].href);
+            const regex = /^[/]ExternalLinkRedirect.*url[=]/i;
+            var replacedVal = links[i].href.replace(regex,'');
+            console.log(replacedVal);
+            links[i].href=replacedVal;
+            console.log(links[i].href);
+        }
+    }*/
+    /*
     $("a").each(function() {
         var hrefVal = $(this).attr('href');
         if(hrefVal != undefined){
@@ -38,8 +62,29 @@ $(document).ready(function(){
             $(this).attr('href', replacedVal);
             $(this).prop("onclick", null);
         }
-    });
+    });*/
 
-  console.log("Tamamdır");
-});
+    console.log("Tamamdır");
+};
+
+
+
+(function() {
+    'use strict';
+
+    if (document.readyState == "complete" || document.readyState == "loaded" || document.readyState == "interactive") {
+        ClearDHFromADS();
+    } else {
+        document.addEventListener("DOMContentLoaded", function(event) {
+            ClearDHFromADS();
+        });
+    }
+})();
+
+
+
+
+
+
+
 
